d94af02c1d429eeea8a8078e7e65fb5154e2eaa7
Ronnie Nyaga <ronnie@livefeather.com>
2019-09-18 20:48:14 +0300

    sendReminderWeekend function

--- handler.js
+++ handler.js
@@ -41,6 +41,47 @@ module.exports.sendReminderDaily = (event, context, callback) => {
   });
 };
 
+module.exports.sendReminderWeekend = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({region:'eu-west-2'});
+  var ses = new AWS.SES();
+  var fs = require('fs');
+
+  var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+  var toAndFromAdress = 'ronnie.nyaga@andela.com'
+  var params = {
+      Destination: {
+          ToAddresses: [toAndFromAdress]
+      },
+      Message: {
+          Body: {
+              Html: {
+                  Charset: "UTF-8",
+                  Data: emailHtml
+              },
+              Text: {
+                  Charset: "UTF-8",
+                  Data: "Here's a weekend Remember that puppies are adorable!!"
+              }
+          },
+          Subject: {
+              Charset: "UTF-8",
+              Data: "Woof Garden Reminder"
+          }
+      },
+      ReplyToAddresses: [toAndFromAdress],
+      Source: toAndFromAdress,
+  };
+
+  ses.sendEmail(params, function(err, data) {
+      // an error occurred
+      if (err) console.log(err, err.stack);
+      // successful response
+      else callback(null, data);
+  });
+};
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
