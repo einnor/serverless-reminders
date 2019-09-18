7ca75818d0ef97f1bbade042490b852e8e82a916
Ronnie Nyaga <ronnie@livefeather.com>
2019-09-18 20:46:31 +0300

    sendReminderDaily function

--- handler.js
+++ handler.js
@@ -1,5 +1,46 @@
 'use strict';
 
+module.exports.sendReminderDaily = (event, context, callback) => {
+  var AWS = require('aws-sdk');
+  AWS.config.update({region:'eu-west-2'});
+  var ses = new AWS.SES();
+  var fs = require('fs');
+
+  var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
+
+  var toAndFromAdress = 'ronnie.nyaga@andela.com'
+  var params = {
+      Destination: {
+          ToAddresses: [toAndFromAdress]
+      },
+      Message: {
+          Body: {
+              Html: {
+                  Charset: "UTF-8",
+                  Data: emailHtml
+              },
+              Text: {
+                  Charset: "UTF-8",
+                  Data: "Remember to continue helping the Woof Garden!"
+              }
+          },
+          Subject: {
+              Charset: "UTF-8",
+              Data: "Woof Garden Reminder"
+          }
+      },
+      ReplyToAddresses: [toAndFromAdress],
+      Source: toAndFromAdress,
+  };
+
+  ses.sendEmail(params, function(err, data) {
+      // an error occurred
+      if (err) console.log(err, err.stack);
+      // successful response
+      else callback(null, data);
+  });
+};
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
